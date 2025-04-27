e064a8b15900af0cff434d49f07233cea5093449
bgarbero <bruno_garbero@hotmail.com>
2025-04-27 08:04:48 -0300

    criando e populando tb_client

---
+++ clientcrud/clientcrud/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Paula Souza', '12345678901', 5500.0, '1990-05-15', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Henrique Lima', '98765432100', 8000.0, '1985-08-22', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Oliveira', '45678912302', 3200.0, '1998-12-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcelo Andrade', '32165498703', 7000.0, '1979-03-30', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrícia Gomes', '65498732104', 4700.0, '1992-09-10', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael Costa', '78912345605', 9100.0, '1987-01-25', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beatriz Mendes', '15975348606', 3800.0, '1995-07-17', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Fernandes', '35715948607', 6000.0, '1991-04-08', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Castro', '25836914708', 4500.0, '1993-11-12', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eduardo Silva', '14725836909', 7300.0, '1983-06-03', 3);
+
